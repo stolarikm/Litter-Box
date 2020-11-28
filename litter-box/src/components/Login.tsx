953025adc921eec7d959d855ae8af2f988f00f71
Mipov <xstolar2@fi.muni.cz>
2020-11-29 00:24:11 +0100

    Fixed width of login card

--- litter-box/src/components/Login.tsx
+++ litter-box/src/components/Login.tsx
@@ -9,6 +9,7 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 
 import { signIn, signUp, useLoggedInUser } from '../firebase/firebase';
+import { Grid } from '@material-ui/core';
 
 const Login: FC = () => {
   const [user, setUser] = useState('');
@@ -21,66 +22,70 @@ const Login: FC = () => {
   }  
 
   return (
-    <Card>
-      <CardContent>
-        <Typography variant='h5' component='h1'>
-          Sign in
-        </Typography>
-        <Typography variant='subtitle1'>Use your Account</Typography>
-        <TextField
-          label='Email'
-          type='email'
-          name='email'
-          fullWidth
-          autoComplete='email'
-          margin='normal'
-          variant='outlined'
-          value={user}
-          onChange={e => setUser(e.target.value)}
-        />
-        <TextField
-          label='Password'
-          type='password'
-          name='password'
-          fullWidth
-          margin='normal'
-          variant='outlined'
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        {error && (
-          <Typography variant='subtitle2' align='left' color='error' paragraph>
-            <b>{error}</b>
-          </Typography>
-        )}
-      </CardContent>
-      <CardActions>
-        <Button
-          variant='text'
-          size='large'
-          color='primary'
-          onClick={async () => {
-            try {
-              await signUp(user, password);
-            } catch (err) {
-              setError(err.message);
-            }
-          }}
-        >
-          Create account
-        </Button>
-        <Button
-          variant='text'
-          size='large'
-          color='primary'
-          onClick={() =>
-            signIn(user, password).catch(err => setError(err.message))
-          }
-        >
-          Login
-        </Button>
-      </CardActions>
-    </Card>
+    <Grid container spacing={10} justify="center">
+      <Grid item>
+        <Card>
+          <CardContent>
+            <Typography variant='h5' component='h1'>
+              Sign in
+            </Typography>
+            <Typography variant='subtitle1'>Use your Account</Typography>
+            <TextField
+              label='Email'
+              type='email'
+              name='email'
+              fullWidth
+              autoComplete='email'
+              margin='normal'
+              variant='outlined'
+              value={user}
+              onChange={e => setUser(e.target.value)}
+            />
+            <TextField
+              label='Password'
+              type='password'
+              name='password'
+              fullWidth
+              margin='normal'
+              variant='outlined'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            {error && (
+              <Typography variant='subtitle2' align='left' color='error' paragraph>
+                <b>{error}</b>
+              </Typography>
+            )}
+          </CardContent>
+          <CardActions>
+            <Button
+              variant='text'
+              size='large'
+              color='primary'
+              onClick={async () => {
+                try {
+                  await signUp(user, password);
+                } catch (err) {
+                  setError(err.message);
+                }
+              }}
+            >
+              Create account
+            </Button>
+            <Button
+              variant='text'
+              size='large'
+              color='primary'
+              onClick={() =>
+                signIn(user, password).catch(err => setError(err.message))
+              }
+            >
+              Login
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
